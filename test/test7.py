38e27d82106fd844dbe9fd59e143c24a22504ca9
mitsuo <m.toyoharap@gmail.com>
2021-03-20 17:07:02 +0900

    最初のコミット

---
+++ test/test7.py
@@ -0,0 +1,9 @@
+import datetime
+print("これはテストです",end="")
+print(100,end="")
+print([0,1,2])
+dt = datetime.datetime.now()
+print(f'ただ今の時間は、{dt.hour}時{dt.minute}分です。',end="")
+dt = datetime.datetime.now()
+import datetime
+print("これはテストです",end="")
